9448de683d4a7d0047c9b95fdb27e3b2cf1e1685
lightgop <lightgop@gmail.com>
2020-07-20 21:30:51 +0900

    log4j 수정 및 controller의 파라미터 수집

--- ex01/src/main/java/kr/co/controller/SampleController.java
+++ ex01/src/main/java/kr/co/controller/SampleController.java
@@ -1,8 +1,11 @@
 package kr.co.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
+import kr.co.domain.SampleDTO;
 import lombok.extern.log4j.Log4j;
 
 @Controller
@@ -14,6 +17,28 @@ public class SampleController {
 	public void basic() {
 		
 		log.info("basic................");
+	}
+	
+	@RequestMapping(value = "/basic", method = { RequestMethod.GET, RequestMethod.POST })
+	public void basicGet() {
+
+		log.info("basic get...................");
+
+	}
+
+	@GetMapping("/basicOnlyGet")
+	public void basicGet2() {
+
+		log.info("basic get only get...................");
+
+	}
+	
+	@GetMapping("/ex01")
+	public String ex01(SampleDTO dto) {
+
+		log.info("" + dto);
+
+		return "ex01";
 	}
 
 }
